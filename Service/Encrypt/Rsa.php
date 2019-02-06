ee128584f6110307ec8b35dcc14a8a39808182f4
vagrant <vagrant@homestead>
2019-02-06 09:45:48 +0000

    encrypt 文件添加基类

--- Service/Encrypt/Rsa.php
+++ Service/Encrypt/Rsa.php
@@ -6,41 +6,27 @@ namespace Service\Encrypt;
  * md5 加密方式的请求处理
  * Class Rsa
  */
-class Rsa extends \Controller implements EncryptInterface
+class Rsa extends BaseEncrypt implements EncryptInterface
 {
-    /**
-     * md5方式加密配置
-     */
-    const CONFIG = [
-        'sql_type' => 2, // 在数据库中的数字代码
-        'symbol' => ['%', '#', '^'], // 使用什么符号拼接
-    ];
-
-    /**
-     * 加密拼接符号
-     * @var string
-     */
-    private $field;
-
-    /**
-     * 设置第三方对应的支付字段
-     * @param $field
-     * @return $this
-     */
-    public function setField($field)
-    {
-        $this->field = $field;
-        return $this;
-    }
-
-
     /**
      * 获取加密处理后的数据
      * @return bool|string
      */
     public function getEncryptPayData()
     {
-        // TODO: Implement getEncryptPayData() method.
+        // 验证数据库字段
+        if (!$this->validateField()) {
+            return false;
+        }
+
+        // 请求第三方支付
+        // 获取整理后的请求第三方的数据
+        $requestData = $this->getRequestDataBySort();
+        if (!$requestData) {
+            return false;
+        }
+
+        return $requestData;
     }
 
 }
